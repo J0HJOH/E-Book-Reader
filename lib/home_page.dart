1d65d7df9ecb6620f938677b4fb6758a1256af24
joan Okereke <joanokereke7@gmail.com>
2023-01-03 12:48:09 -0800

    Worked on adding the reading Screen

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:my_e_book_reader/reader.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -21,12 +24,34 @@ class _HomePageState extends State<HomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
-          FilePickerResult? result = await
-          FilePicker.platform.pickFiles();
+          await _selectFile(context);
 
         },
         child: Icon(Icons.folder_open),
       ),
     );
+  }
+
+  Future<void> _selectFile(BuildContext context) async {
+     FilePickerResult? result = await
+    FilePicker.platform.pickFiles(
+        allowedExtensions: ["pdf"],
+      type: FileType.custom
+    );
+    if(result != null){
+      // User selected the file
+      //create file instance
+      File docFile = File(result.files[0].path!);
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context)=>ReaderPage(document : docFile))
+      );
+    }else{
+      //user cancelled selection
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("File Selection cancelled")
+          )
+      );
+    }
   }
 }
